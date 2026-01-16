ea56d31dd21b920fd13176b5fb51ecef6900656d
hsz <hsz@meta.com>
2026-01-15 23:33:54 -0800

    unused stuff

--- dl3/android/DeepLabV3Demo/app/src/main/java/org/pytorch/executorchexamples/dl3/TensorImageUtils.java
+++ dl3/android/DeepLabV3Demo/app/src/main/java/org/pytorch/executorchexamples/dl3/TensorImageUtils.java
@@ -9,13 +9,11 @@
 package org.pytorch.executorchexamples.dl3;
 
 import android.graphics.Bitmap;
-import android.util.Log;
 import java.nio.FloatBuffer;
 import org.pytorch.executorch.Tensor;
 
 /**
- * Contains utility functions for {@link Tensor} creation from {@link android.graphics.Bitmap} or
- * {@link android.media.Image} source.
+ * Contains utility functions for {@link Tensor} creation from {@link android.graphics.Bitmap}.
  */
 public final class TensorImageUtils {
 
@@ -40,19 +38,23 @@ public final class TensorImageUtils {
   }
 
   /**
-   * Writes tensor content from specified {@link android.graphics.Bitmap}, normalized with specified
-   * in parameters mean and std to specified {@link java.nio.FloatBuffer} with specified offset.
-   *
-   * @param bitmap {@link android.graphics.Bitmap} as a source for Tensor data
-   * @param x - x coordinate of top left corner of bitmap's area
-   * @param y - y coordinate of top left corner of bitmap's area
-   * @param width - width of bitmap's area
-   * @param height - height of bitmap's area
-   * @param normMeanRGB means for RGB channels normalization, length must equal 3, RGB order
-   * @param normStdRGB standard deviation for RGB channels normalization, length must equal 3, RGB
-   *     order
+   * Creates new {@link Tensor} from specified area of {@link android.graphics.Bitmap}, normalized
+   * with specified in parameters mean and std.
    */
-  public static void bitmapToFloatBuffer(
+  private static Tensor bitmapToFloat32Tensor(
+      final Bitmap bitmap,
+      int x,
+      int y,
+      int width,
+      int height,
+      float[] normMeanRGB,
+      float[] normStdRGB) {
+    final FloatBuffer floatBuffer = Tensor.allocateFloatBuffer(3 * width * height);
+    bitmapToFloatBuffer(bitmap, x, y, width, height, normMeanRGB, normStdRGB, floatBuffer, 0);
+    return Tensor.fromBlob(floatBuffer, new long[] {1, 3, height, width});
+  }
+
+  private static void bitmapToFloatBuffer(
       final Bitmap bitmap,
       final int x,
       final int y,
@@ -62,9 +64,6 @@ public final class TensorImageUtils {
       final float[] normStdRGB,
       final FloatBuffer outBuffer,
       final int outBufferOffset) {
-    checkOutBufferCapacity(outBuffer, outBufferOffset, width, height);
-    checkNormMeanArg(normMeanRGB);
-    checkNormStdArg(normStdRGB);
     final int pixelsCount = height * width;
     final int[] pixels = new int[pixelsCount];
     bitmap.getPixels(pixels, 0, width, x, y, width, height);
@@ -78,57 +77,6 @@ public final class TensorImageUtils {
       outBuffer.put(outBufferOffset + i, (r - normMeanRGB[0]) / normStdRGB[0]);
       outBuffer.put(outBufferOffset + offset_g + i, (g - normMeanRGB[1]) / normStdRGB[1]);
       outBuffer.put(outBufferOffset + offset_b + i, (b - normMeanRGB[2]) / normStdRGB[2]);
-    }
-  }
-
-  /**
-   * Creates new {@link Tensor} from specified area of {@link android.graphics.Bitmap}, normalized
-   * with specified in parameters mean and std.
-   *
-   * @param bitmap {@link android.graphics.Bitmap} as a source for Tensor data
-   * @param x - x coordinate of top left corner of bitmap's area
-   * @param y - y coordinate of top left corner of bitmap's area
-   * @param width - width of bitmap's area
-   * @param height - height of bitmap's area
-   * @param normMeanRGB means for RGB channels normalization, length must equal 3, RGB order
-   * @param normStdRGB standard deviation for RGB channels normalization, length must equal 3, RGB
-   *     order
-   */
-  public static Tensor bitmapToFloat32Tensor(
-      final Bitmap bitmap,
-      int x,
-      int y,
-      int width,
-      int height,
-      float[] normMeanRGB,
-      float[] normStdRGB) {
-    checkNormMeanArg(normMeanRGB);
-    checkNormStdArg(normStdRGB);
-
-    final FloatBuffer floatBuffer = Tensor.allocateFloatBuffer(3 * width * height);
-    bitmapToFloatBuffer(bitmap, x, y, width, height, normMeanRGB, normStdRGB, floatBuffer, 0);
-    return Tensor.fromBlob(floatBuffer, new long[] {1, 3, height, width});
-  }
-
-  private static void checkOutBufferCapacity(
-      FloatBuffer outBuffer, int outBufferOffset, int tensorWidth, int tensorHeight) {
-    if (outBufferOffset + 3 * tensorWidth * tensorHeight > outBuffer.capacity()) {
-      throw new IllegalStateException("Buffer underflow");
-    }
-  }
-
-  private static void checkTensorSize(int tensorWidth, int tensorHeight) {
-    if (tensorHeight <= 0 || tensorWidth <= 0) {
-      throw new IllegalArgumentException("tensorHeight and tensorWidth must be positive");
-    }
-  }
-
-  private static void checkRotateCWDegrees(int rotateCWDegrees) {
-    if (rotateCWDegrees != 0
-        && rotateCWDegrees != 90
-        && rotateCWDegrees != 180
-        && rotateCWDegrees != 270) {
-      throw new IllegalArgumentException("rotateCWDegrees must be one of 0, 90, 180, 270");
     }
   }
 
